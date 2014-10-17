473686a76ba1c6d467b5467befecf88fb441cbb2
wu fenglong <wufenglong1980@139.com>
2014-10-17 09:50:11 +0800

    first commit/

---
+++ src/com/guagua/face/FirstItemMaxListView/MainActivity.java
@@ -0,0 +1,15 @@
+package com.guagua.face.FirstItemMaxListView;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wufl on 14-10-17.
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
